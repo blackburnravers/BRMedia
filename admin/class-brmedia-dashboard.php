5d2e6dc164b6af48505b15b199f5c6afeccd3cb8
blackburnravers <rhysc2101@gmail.com>
2025-03-29 12:22:04 +0000

    Update class-brmedia-dashboard.php

--- admin/class-brmedia-dashboard.php
+++ admin/class-brmedia-dashboard.php
@@ -34,6 +34,30 @@ class BRMedia_Dashboard_Page {
             <div class="brmedia-dashboard-grid">
                 <?php do_action( 'brmedia_admin_dashboard' ); ?>
             </div>
+
+            <div class="brmedia-dashboard-sections">
+                <div class="brmedia-box-grid">
+                    <div class="brmedia-box">
+                        <h3><i class="fas fa-music"></i> <?php _e( 'Add New Track', 'brmedia' ); ?></h3>
+                        <p><a href="<?php echo admin_url('post-new.php?post_type=brmedia_music'); ?>" class="button button-primary"><?php _e( 'Add Music', 'brmedia' ); ?></a></p>
+                    </div>
+
+                    <div class="brmedia-box">
+                        <h3><i class="fas fa-video"></i> <?php _e( 'Add New Video', 'brmedia' ); ?></h3>
+                        <p><a href="<?php echo admin_url('post-new.php?post_type=brmedia_video'); ?>" class="button button-primary"><?php _e( 'Add Video', 'brmedia' ); ?></a></p>
+                    </div>
+
+                    <div class="brmedia-box">
+                        <h3><i class="fas fa-chart-pie"></i> <?php _e( 'View Stats', 'brmedia' ); ?></h3>
+                        <p><a href="<?php echo admin_url('admin.php?page=brmedia-stats'); ?>" class="button"><?php _e( 'View Stats', 'brmedia' ); ?></a></p>
+                    </div>
+
+                    <div class="brmedia-box">
+                        <h3><i class="fas fa-code"></i> <?php _e( 'Shortcodes', 'brmedia' ); ?></h3>
+                        <p><a href="<?php echo admin_url('admin.php?page=brmedia-shortcodes'); ?>" class="button"><?php _e( 'Shortcode List', 'brmedia' ); ?></a></p>
+                    </div>
+                </div>
+            </div>
         </div>
         <?php
     }
